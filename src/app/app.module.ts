d27c06334dc9f80812b3923da6b51ef9b28f2159
ginhanlabsgit config --global user.name ginhanlabsgit config --global user.name ginhanlabs <ginhanlabs@gmail.com>
2020-07-28 22:32:19 -0400

    fixed init routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app.routing';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
-import { NavigationComponent } from './component/navigation/navigation.component';
-import { DetailsComponent } from './component/details/details.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { DetailsComponent } from './components/details/details.component';
 
 @NgModule({
   declarations: [
