79fed80396f6852eacc176c680ff4c094ee7cbb4
ginhanlabsgit config --global user.name ginhanlabsgit config --global user.name ginhanlabs <ginhanlabs@gmail.com>
2020-08-18 19:37:38 -0400

    moving nav to main

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule} from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
 import { DetailsComponent } from './components/details/details.component';
+import { MainContentComponent } from './components/main-content/main-content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NavigationComponent,
-    DetailsComponent
+    DetailsComponent,
+    MainContentComponent
   ],
   imports: [
     BrowserModule,
