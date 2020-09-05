55563fe66389416b9926b4d0cf73d690a6143a84
ginhanlabsgit config --global user.name ginhanlabsgit config --global user.name ginhanlabs <ginhanlabs@gmail.com>
2020-09-04 21:36:01 -0400

    begin adding a form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,19 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { MainContentComponent } from './components/main-content/main-content.component';
+import { AddComponent } from './add/add.component';
 
 
 const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'main/:category', component: MainContentComponent },
+    { path: 'add', component: AddComponent },
     { path: '',   redirectTo: '/home', pathMatch: 'full' }
   ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  declarations: [AddComponent]
 })
 export class AppRoutingModule { }
